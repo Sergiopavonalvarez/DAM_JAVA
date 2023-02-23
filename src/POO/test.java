b1911b6a916505f2b3310e2bfd2a9acfbb5d8f74
Sergiopavonalvarez <118288663+Sergiopavonalvarez@users.noreply.github.com>
2023-02-23 11:04:05 +0100

    actualizado

---
+++ src/POO/test.java
@@ -0,0 +1,22 @@
+package POO;
+
+import java.util.Scanner;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		
+		Rectangulo in = new Rectangulo(0,0);
+		in.getAlto();
+		in.getAncho();
+		System.out.println(in.getRectangulo());
+		
+		
+
+
+		
+
+	}
+
+}
